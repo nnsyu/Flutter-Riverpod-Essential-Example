f02bb05b41676a9a91cc8cafb36110ee2412430e
nnnsyu <nnnsyu@gmail.com>
2024-07-16 16:05:05 +0900

    StateProvider 끝

---
+++ state_provider/lib/pages/auto_dispose_family/auto_dispose_family_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final autoDisposeFamilyCounterProvider =
+    StateProvider.autoDispose.family<int, int>((ref, initialValue) {
+  ref.onDispose(() =>
+      print('[autoDisposeFamilyCounterProvider($initialValue)] disposed'));
+  return initialValue;
+});
---
+++ state_provider/lib/pages/basic/basic_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'basic_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$ageHash() => r'834458b9550576c9fb3c8c53543ce37ce582efbf';
+
+/// See also [age].
+@ProviderFor(age)
+final ageProvider = Provider<String>.internal(
+  age,
+  name: r'ageProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$ageHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AgeRef = ProviderRef<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
